81a83f540bafbd7a74d84f136e22c7912366fff9
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-09-05 10:52:46 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // SınıfAdı objeAdı = new SınıfAdı();
+
+		/*HesapMakinesi makine1 = new HesapMakinesi();
+		makine1.sayi1 = 10;
+		makine1.sayi2 = 20;
+		System.out.println(makine1.sayi1);
+
+		HesapMakinesi makine2 = new HesapMakinesi();
+		makine2.sayi1 = 18;
+		System.out.println(makine2.sayi1);*/
+
+        // Constructor (Yapıcı)
+        /*HesapMakinesi makine1;
+        makine1 = new HesapMakinesi(1,3);
+        System.out.println(makine1.sayi1 + "-" + makine1.sayi2);
+
+        makine1.sayi1 = 5;
+        System.out.println(makine1.sayi1 + "-" + makine1.sayi2);
+
+        HesapMakinesi makine2 = new HesapMakinesi(10,5);
+        System.out.println(makine2.toplama());
+        System.out.println(makine2.cıkarma());
+        System.out.println(makine2.carpma());
+        System.out.println(makine2.bölme());*/
+
     }
 }
